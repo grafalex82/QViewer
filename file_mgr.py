6dc9aa9073a9ad0acc2110892a08525ed45fbe8b
Oleksandr Masliuchenko <o.masliuchenko@globallogic.com>
2024-10-30 21:24:55 +0200

    Rename variables for better readability

--- file_mgr.py
+++ file_mgr.py
@@ -5,10 +5,10 @@ class FileMgr:
         self.reset_current_dir()
 
     def reset_current_dir(self):
-        self.__current_file_index = None
-        self.__current_directory = None
-        self.__current_directory_files = []
-        self.__current_directory_subdirs = []
+        self.__file_index = None
+        self.__directory = None
+        self.__directory_files = []
+        self.__directory_subdirs = []
 
     def load_file(self, fname):
         if not fname:
@@ -19,10 +19,10 @@ class FileMgr:
         self.load_directory(base_dir)
 
         base_name = os.path.basename(os.path.realpath(fname))
-        if not base_name in self.__current_directory_files:
+        if not base_name in self.__directory_files:
             raise RuntimeError(f"File {fname} not found")
-        self.__current_file_index = self.__current_directory_files.index(base_name)
-        print(f"Setting current index to {self.__current_file_index}")
+        self.__file_index = self.__directory_files.index(base_name)
+        print(f"Setting current index to {self.__file_index}")
 
     def load_directory(self, directory):
         if not directory:
@@ -32,23 +32,24 @@ class FileMgr:
         for entry in os.listdir(directory):
             full_path = os.path.join(directory, entry)
             if os.path.isdir(full_path):
-                self.__current_directory_subdirs.append(entry)
+                self.__directory_subdirs.append(entry)
                 print(f"Directory: {entry}")
             else:
-                self.__current_directory_files.append(entry)
+                self.__directory_files.append(entry)
                 print(f"File: {entry}")
         
-        self.__current_directory = directory
-        self.__current_directory_files.sort()
-        self.__current_directory_subdirs.sort()
-        print(f"Setting current directory to {self.__current_directory}")
-        print(f"Files: {self.__current_directory_files}")
-        print(f"Directories: {self.__current_directory_subdirs}")
+        self.__directory = directory
+        self.__directory_files.sort()
+        self.__directory_subdirs.sort()
+        self.__file_index = 0
+        print(f"Setting current directory to {self.__directory}")
+        print(f"Files: {self.__directory_files}")
+        print(f"Directories: {self.__directory_subdirs}")
 
             
     def current_file(self):
-        if self.__current_file_index != None:
-            fname = self.__current_directory_files[self.__current_file_index]
-            return os.path.join(self.__current_directory, fname)
+        if self.__file_index != None:
+            fname = self.__directory_files[self.__file_index]
+            return os.path.join(self.__directory, fname)
         return None
 
