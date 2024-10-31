359f7a89e28dfb39a702b607dfffdc51ef3b5e9f
Oleksandr Masliuchenko <o.masliuchenko@globallogic.com>
2024-10-31 20:57:45 +0200

    Add prev/next image/directory actions

--- src/main.py
+++ src/main.py
@@ -48,6 +48,30 @@ class ImageLoaderApp(QMainWindow):
         exit_action.triggered.connect(self.close)
         file_menu.addAction(exit_action)
 
+        # Create the View menu
+        view_menu = menu_bar.addMenu("View")
+
+        # Create navigation actions
+        prev_action = QAction("Previous Image", self)
+        prev_action.setShortcut("Left")
+        prev_action.triggered.connect(self.prev_image)
+        view_menu.addAction(prev_action)
+
+        next_action = QAction("Next Image", self)
+        next_action.setShortcut("Right")
+        next_action.triggered.connect(self.next_image)
+        view_menu.addAction(next_action)
+
+        prev_dir_action = QAction("Previous Directory", self)
+        prev_dir_action.setShortcut("Ctrl-Left")
+        prev_dir_action.triggered.connect(self.prev_dir)
+        view_menu.addAction(prev_dir_action)
+
+        next_dir_action = QAction("Next Directory", self)
+        next_dir_action.setShortcut("Ctrl-Right")
+        next_dir_action.triggered.connect(self.next_dir)
+        view_menu.addAction(next_dir_action)
+
 
     def open_file(self):
         # Open a file dialog to select an image
@@ -71,6 +95,27 @@ class ImageLoaderApp(QMainWindow):
         self.label.setPixmap(pixmap)
         self.label.setAlignment(Qt.AlignCenter)
         self.resize(pixmap.width(), pixmap.height())
+
+
+    def prev_image(self):
+        if self.mgr.prev():
+            self.display_image(self.mgr.current_file())
+
+
+    def next_image(self):
+        if self.mgr.next():
+            self.display_image(self.mgr.current_file())
+
+
+    def prev_dir(self):
+        if self.mgr.prev_dir():
+            self.display_image(self.mgr.current_file())
+
+
+    def next_dir(self):
+        if self.mgr.next_dir():
+            self.display_image(self.mgr.current_file())
+
 
 if __name__ == '__main__':
     image_path = sys.argv[1] if len(sys.argv) > 1 else None
